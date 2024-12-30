ab1c6b0fa1e2dcc49c8d85bb92c455090f67a174
AnkitKathale <kathaleankit4@gmail.com>
2024-12-30 19:07:25 +0530

    Create Variable2.java
    Learned some basic String class methods and string as a non primitive data type

---
+++ src/javaPackage/Variable2.java
@@ -0,0 +1,11 @@
+package javaPackage;
+
+public class Variable2 {
+    public static void main(String[] args) {
+        String a="hello java";  //String type local variable
+        System.out.println(a);
+        System.out.println(a.length());//length of string
+        System.out.println(a.charAt(3));//prints element of string at that indes
+        System.out.println(a.concat("allah"));//concats string with given string
+    }
+}
