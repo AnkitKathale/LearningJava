c2b17d413380d65e9ae738504040d22bc379ac80
AnkitKathale <kathaleankit4@gmail.com>
2024-12-30 19:45:42 +0530

    more clarity about variables

---
+++ src/javaPackage/Variable9.java
@@ -0,0 +1,18 @@
+package javaPackage;
+
+public class Variable9 {
+    public static void main(String[] args) {
+        int a;//declaration of variable
+        a =10;//defining variable
+        System.out.println(a);//calling variable a
+
+        char b='h';
+        System.out.println(b);//h
+
+        char c='b';
+        System.out.println(c);//b
+
+        String sallu="hello Mrugaya";
+        System.out.println(sallu.length());
+    }
+}
