cf0ccc600af331943fd2da42502132bcfe34197b
AnkitKathale <kathaleankit4@gmail.com>
2024-12-30 17:55:53 +0530

    Comments and method
    Added some methods for explaination and one non static method

--- src/javaPackage/FirstProgram.java
+++ src/javaPackage/FirstProgram.java
@@ -5,15 +5,18 @@ public class FirstProgram {
     static int b=5;//static variable
     public static void main(String[] args) {        //Main method
         System.out.println("Hello world");          //print statement
-        aMethod();//Calling method
-        bMethod();//Calling method
+        aMethod();//Calling a method
+        bMethod();//Calling  b method
     }
     public static void aMethod(){       //user defined method
         int c=50;
-        System.out.println(c);
+        System.out.println(c);          //calling variable
     }
     public static void bMethod(){       //user defined method
         int d=15;
         System.out.println(d);
+    }
+    public  void cMethod(){
+        System.out.println("Non static method we have to create object");
     }
 }
