7ff9be61b34cb5b218860884a793eff746a01240
AnkitKathale <kathaleankit4@gmail.com>
2024-12-30 18:57:41 +0530

    Variables
    learned about variable declaration ,defination

---
+++ src/javaPackage/Variable1.java
@@ -0,0 +1,24 @@
+package javaPackage;
+
+public class Variable1 {
+    public static void main(String[] args) {
+        int a =5; //Defining  of local variable
+        int b;    //Declaration local variable
+        System.out.println(a);
+        b=200;
+        System.out.println(b);
+        b=300;
+        System.out.println(b);
+        char c ='a';
+        System.out.println(c);
+        boolean d=true;
+
+        System.out.println(d);//true
+
+        d=false;
+        System.out.println(3>15);
+
+        boolean j= a <b;
+        System.out.println(j);
+    }
+}
