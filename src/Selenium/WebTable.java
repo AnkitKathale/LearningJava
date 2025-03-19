b0260ac8806acd728102cfdc63b1410bb35b48c0
AnkitKathale <95280757+AnkitKathale@users.noreply.github.com>
2025-03-19 16:38:55 +0530

    Web table
    learnt how to handle web table and anchor tags(embedded links)

---
+++ src/Selenium/WebTable.java
@@ -0,0 +1,34 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class WebTable {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Ankit\\New folder\\LearningJava\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://money.rediff.com/gainers");
+        List<WebElement> CompanyName = driver.findElements(By.xpath("//table[@class='dataTable']/tbody/tr/td[1]/a")); //lists all companies in that column
+        List<WebElement> CompanyPrice = driver.findElements(By.xpath("//table[@class='dataTable']/tbody/tr/td[4]"));
+//        System.out.println(CompanyName.size());
+//        System.out.println(CompanyPrice.size());
+
+        String expResult="I P Rings Ltd.";
+
+        for(int i =0;i<CompanyName.size();i++){
+            if(CompanyName.get(i).getText().equalsIgnoreCase(expResult)){
+                System.out.println(CompanyName.get(i).getText()+"======"+CompanyPrice.get(i).getText());
+                CompanyName.get(i).click();
+                driver.getTitle();
+                break;
+
+            }
+        }
+
+
+    }
+}
